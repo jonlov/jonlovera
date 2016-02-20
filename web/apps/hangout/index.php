fda5f4e77927ced6eea462a0e4d9bd258f5b90d1
Jonathan Lovera <jonlov@Jonathans-MacBook-Pro.local>
2016-02-20 16:04:36 +1100

    new5

--- web/apps/hangout/index.php
+++ web/apps/hangout/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
-    <title></title>
+    <title>Example Hangout App - Jonathan Lovera</title>
     <!-- CSS  -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
@@ -20,7 +20,7 @@
         <div class="container">
             <br>
             <br>
-            <h1 class="header center green-text">Starter Hangout App</h1>
+            <h1 class="header center green-text">Example Hangout App</h1>
             <div class="row center">
                 <script src="https://apis.google.com/js/platform.js" async defer></script>
                 <g:hangout render="createhangout" data-render="createhangout"
