d03b2cdc443a67095f0968aba65bcf08ef662bbe
Jonathan Lovera <jonlov@192-168-1-4.tpgi.com.au>
2016-05-16 05:13:01 +1000

    Pre final 5 (email send)

--- web/api/contact/index.php
+++ web/api/contact/index.php
@@ -1,35 +1,7 @@
 <?php 
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    require 'PHPMailerAutoload.php';
-
-    $mail = new PHPMailer;
-
-    //$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'host.caracashosting40.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'contact@yesymilano.com';                 // SMTP username
-    $mail->Password = 'Milano03';                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 465;                                    // TCP port to connect to
-
-    $mail->setFrom('contact@yesymilano.com', 'Mailer');
-    $mail->addAddress('rockjonathan18@gmail.com', 'Jon');     // Add a recipient
-
-    $mail->isHTML(true);                                  // Set email format to HTML
-
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    if(!$mail->send()) {
-        echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-    } else {
         echo 'Message has been sent';
-    }
-
+        
   } else {
     header('HTTP/1.0 400 Bad Request');
     die('Bad request.');     
